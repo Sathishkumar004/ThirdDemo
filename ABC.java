e1ac59591a88b897522f2b85b868602e6a92a59f
Sathishkumar004 <136798658+Sathishkumar004@users.noreply.github.com>
2023-08-28 13:54:34 +0530

    Update ABC.java

--- ABC.java
+++ ABC.java
@@ -2,6 +2,9 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 	System.out.println("Hello World 2!");
+	int a=1;
+	int b=2;
+	System.out.println(a);
     }
 }
 
